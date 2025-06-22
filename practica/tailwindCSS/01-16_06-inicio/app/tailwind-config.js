02c7080ecaa216a7824976af3da4310af2344660
kinrgdev <jrg83x@gmail.com>
2025-06-22 10:10:03 +0200

    añadido la carpeta para practicar y estudiar tailwind. Y ligera modificación de pelis.html de eG. Chuleta git cambiada completamante

---
+++ practica/tailwindCSS/01-16_06-inicio/app/tailwind-config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+  theme: {
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+      tektur: ['Tektur', 'sans-serif'],
+      bangers: ['Bangers', 'system-ui']
+    },
+    extend: {
+      colors: {
+        body: '#171818',
+        blueviolet: '#8a2be2',
+        chocolate: '#d2691e',
+        tomato: '#ff6347',
+        text_primary: '#ccc',
+      }
+    }
+  }
+}
