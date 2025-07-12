2703dfe1e65c3a9d2951cf2666f6395cb7d8261d
kinrgdev <jrg83x@gmail.com>
2025-07-12 13:24:25 +0200

    Base Tailwind

---
+++ practica/tailwind/tailwind-projects/my-app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html",  // Escanea todos los HTML
+    "./src/**/*.js" // Y archivos JS si los tienes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
