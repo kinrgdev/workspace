3a1d6f853aa2dff9136a2fc5935381890af68cf4
kinrgdev <jrg83x@gmail.com>
2025-07-15 20:00:07 +0200

    hasta app ls (8).

---
+++ cursos/node_midu/6.ls.C.js
@@ -0,0 +1,16 @@
+//Con try-catch
+
+const fs = require('node:fs/promises')
+
+fs.readdir('.').then(files => { //LEER EL DIRECTORIO ACTUAL
+    files.forEach(file => {
+        console.log(file)
+    })
+})
+
+.catch(err => {
+    if (err){
+        console.error('Error al leer el directorio:', err)
+        return;
+    }
+})
